b163d30459f5e8f896f523bc5140b8f2032d26b8
Avishay Ben Natan <avishay@flexi-core.com>
2019-08-17 06:15:39 +0300

    Added InstallationTask Inheritance to support 'old' installer modules with minimal changes

--- src/main/java/com/flexicore/installer/interfaces/IInstallationTask.java
+++ src/main/java/com/flexicore/installer/interfaces/IInstallationTask.java
@@ -2,15 +2,44 @@ package com.flexicore.installer.interfaces;
 
 import com.flexicore.installer.model.InstallationContext;
 import com.flexicore.installer.model.InstallationResult;
+import com.flexicore.installer.model.MergeResult;
+import com.flexicore.installer.model.Parameters;
 import org.pf4j.ExtensionPoint;
 
 import java.util.Set;
 
 
 public interface IInstallationTask extends ExtensionPoint {
-
+    /**
+     *
+     * @param installationContext
+     * @return
+     */
     InstallationResult install(InstallationContext installationContext);
+
+    /**
+     *  ID must be unique across all plugins
+     * @return
+     */
     String getId();
+
+    /**
+     * get a description of installer plug-in
+     * @return
+     */
+    String getInstallerDescription();
+
+    /**
+     * get a list of plugins that must run before this one runs.
+     * @return
+     */
     Set<String> getPrerequisitesTask();
+
+    /**
+     * returns a Parameters instance with the required parameters added , system will
+     * @param parameters
+     * @return MergeResult
+     */
+    MergeResult mergeParameters(Parameters parameters);
 
 }
